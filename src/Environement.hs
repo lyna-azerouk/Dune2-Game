d615b4239e7c07595ea48ad668f6b55a33b33ef5
giugiu <61977659+giugiu-stella@users.noreply.github.com>
2023-05-03 18:01:29 +0200

    correction de l avancement

--- src/Environement.hs
+++ src/Environement.hs
@@ -4,7 +4,8 @@ import Data.Ord
 import Data.List
 import Carte
 import Control.Monad (when)
-
+import Control.Monad (foldM)
+import System.Random
 
 newtype JoueurId = JoueurId Int deriving (Eq, Show)
 data Joueur = Joueur{
@@ -22,7 +23,13 @@ jname joueur@(Joueur _ name _) = name
 jcredit::Joueur -> Int
 jcredit joueur@(Joueur _ _ credit) = credit
 
-newtype BatId = BatId Int
+newtype BatId = BatId Int deriving (Eq, Show)
+
+instance Ord BatId where
+  (BatId id1) < (BatId id2) = id1 < id2
+  (BatId id1) <= (BatId id2) = id1 <= id2
+  (BatId id1) > (BatId id2) = id1 > id2
+  (BatId id1) >= (BatId id2) = id1 >= id2
 
 data TypeBatiment = QG Int -- Int = energie_prod
     | Raffinerie Int -- Int = energie_conso
@@ -50,7 +57,7 @@ bpv batiment@(Batiment _ _ _ _ pvb _)= pvb
 bprix:: Batiment -> Int
 bprix batiment@(Batiment _ _ _ _ _ prixb)= prixb
 
-bid:: Batiment -> Int
+bid:: Batiment -> BatId
 bid batiment@(Batiment _ _ _ id _ _)= id
 
 btype:: Batiment -> String
@@ -62,7 +69,7 @@ btype batiment@(Batiment _ _ typeb _ _ _)= case typeb of
     _-> "error pas de type bâtiment"
     
 newtype UniteId = UniteId Int 
-data TypeUnite = Collecteur 
+data TypeUnite = Collecteur Int
     |Combattant 
     deriving (Eq, Show)
 
@@ -85,7 +92,7 @@ uproprio unite@(Unite _ propriou _ _ _ _) = propriou
 
 utype :: Unite -> String
 utype unite@(Unite _ _ typeu _ _ _) = case typeu of
-    Collecteur -> "collecteur"
+    Collecteur _ -> "collecteur"
     Combattant -> "combattant"
 
 upid :: Unite -> UniteId
@@ -97,6 +104,11 @@ upv unite@(Unite _ _ _ _ pvu _) = pvu
 uordres :: Unite -> M.Map Int TypeOrdres
 uordres unite@(Unite _ _ _ _ _ ordres) = ordres
 
+ressouceCollecteur :: Unite -> Int
+ressouceCollecteur unite@(Unite _ _ typeu _ _ _) =
+  case typeu of
+    Collecteur n -> n
+    _ -> 0
 
 data Environement= Environement{
     joueurs:: [Joueur],
@@ -116,7 +128,7 @@ cherche_Case_Batiment :: Coord -> M.Map BatId Batiment -> [Batiment]
 cherche_Case_Batiment coord bats =
   map snd $ filter (\(_, bat) -> caseBatimentCoord bat coord) (M.toList bats)
   where
-    caseBatimentCoord (Batiment c _ _ _ _) coordb = c == coordb
+    caseBatimentCoord (Batiment c _ _ _ _ _) coordb = c == coordb
 
 cherche_Case_Unite :: Coord ->M.Map UniteId Unite -> [Unite]
 cherche_Case_Unite coord unis =
@@ -131,7 +143,7 @@ prop_environnement_1 env@(Environement joueurs _ unites batiments) =
                                                 Just _ -> True
                                                 Nothing -> False) (M.toList unites)
     &&
-    all (\(_, bat@( Batiment _ propriob _ _ _)) ->case chercheJoueur joueurs propriob of
+    all (\(_, bat@( Batiment _ propriob _ _ _ _)) ->case chercheJoueur joueurs propriob of
                                                 Just _ -> True
                                                 Nothing -> False) (M.toList batiments)
 
@@ -168,41 +180,57 @@ prop_environnement_4 (Environement _ carte@(Carte _ _ contenu) unites batiments)
                     _->False
         _ -> True) (M.toList contenu)
 
+randomCoord :: Int -> Int -> [Coord] -> IO Coord
+randomCoord maxX maxY coords = do
+  x <- randomRIO (0, maxX)
+  y <- randomRIO (0, maxY)
+  let coord = Coord x y
+  if coord `elem` coords then randomCoord maxX maxY coords else return coord
 
-
-
-randomCoord :: Int -> Int -> [Coord] -> Coord
-randomCoord maxX maxY coords =
-  let x = randomRIO (0, maxX)
-      y = randomRIO (0, maxY)
-      coord = Coord <$> x <*> y
-  in if coord `elem` coords then randomCoord maxX maxY coords else coord
-
-whileLoop :: Carte -> [Coord] -> Coord
+whileLoop :: Carte -> [Coord] -> IO Coord
 whileLoop carte coords = do
-  let coord = randomCoord (cartel carte) (carteh carte) coords
-  let recup = getTerrain coord carte
+  coord <- randomCoord (cartel carte) (carteh carte) coords
+  let recup = getCase coord carte
   case recup of
-    Just Herbe -> coord
+    Just Herbe -> return coord
     _ -> whileLoop carte (coord : coords)
 
+creerListeBatConst :: [Batiment] -> M.Map BatId Batiment -> M.Map BatId Batiment
+creerListeBatConst bats res = foldl (\acc bat@(Batiment _ _ _ idb _ _) -> M.insert idb bat acc) res bats
+
+
 -- constructeur intelligent Environnement 
-smartConst_env :: Carte -> [Joueur] -> Environement
-smartConst_env carte listejoueurs = 
-  let coords = [] in
-  let batiments = foldl (\bats j -> 
-                          let coord = whileLoop carte coords 
-                              bat = Batiment { coorb = coord 
-                                             , propriob= j
+smartConst_env :: Carte -> [Joueur] -> IO Environement
+smartConst_env carte listejoueurs = do
+  let coords = [] 
+  (batiments, _, _) <- foldM (\(bats, ns, cs) j -> do
+                          coord <- whileLoop carte cs
+                          let bat = Batiment { coordb = coord 
+                                             , propriob= jid j
                                              , typeb = QG 15
-                                             , idb = BatId n
+                                             , idb = BatId ns
                                              , pvb = 30
-                                             , prixb = 0 } 
-                                newcoords = coord:coords
-                                newn=n+1  
-                          in bat:bats) [] listejoueurs
-  in Environement { joueurs = listejoueurs
-                  , ecarte = carte
-                  , unites = M.empty
-                  , batiments = batiments }
-  where n = 1
+                                             , prixb = 0 }  
+                          return (bat:bats, ns+1, coord:cs)) ([], 1, coords) listejoueurs
+  return $ Environement { joueurs = listejoueurs
+                        , ecarte = carte
+                        , unites = M.empty
+                        , batiments = creerListeBatConst batiments M.empty}
+
+actionRaffinerie :: Unite -> Batiment -> (Unite, Int)
+actionRaffinerie unite@(Unite _ _ (Collecteur n) _ _ _) _ =
+  let nbrRessources = ressouceCollecteur unite
+  in (unite { typeu = Collecteur 0 }, nbrRessources)
+
+propr_pre_actionRaffinerie :: Unite -> Bool
+propr_pre_actionRaffinerie (Unite _ _ (Collecteur n) _ _ _) = n > 0
+propr_pre_actionRaffinerie _ = False
+
+propr_post_actionRaffinerie :: Unite -> Bool
+propr_post_actionRaffinerie (Unite _ _ (Collecteur n) _ _ _) = n == 0
+propr_post_actionRaffinerie _ = False
+
+invariant_actionRaffinerie :: Unite -> Bool
+invariant_actionRaffinerie (Unite _ _ Combattant _ _ _) = False
+invariant_actionRaffinerie (Unite _ _ (Collecteur _) _ _ _) = True
+invariant_actionRaffinerie _ = False
