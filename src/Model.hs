ac08e8a20a77465168a27849740aaf31dce66230
Lynadjdjdjd <114083371+Lynadjdjdjd@users.noreply.github.com>
2023-05-07 17:12:26 +0200

    deplacement

--- src/Model.hs
+++ src/Model.hs
@@ -1,49 +1,49 @@
 
 module Model where
 
---import SDL
+import SDL
 
 import Keyboard (Keyboard)
 import qualified Keyboard as K
+import Environnement ( Envi)
 
 
 import Data.Int (Int32)
 import qualified Data.Int as I
 
-data GameState = GameState { persoX :: Int
-                           , persoY :: Int
-                           , speed :: Int }
+data GameState = GameState  { carte :: Carte        -- carte actuelle
+                      , envi :: Envi          -- environnement actuel
+                      , keyboard :: Keyboard  -- l’etat du clavier
+                      ,persoX :: Int
+                      , persoY :: Int
+                      , speed :: Int 
+                      }
   deriving (Show)
 
 
--- FAUT DEFINIR LES REGLES DU JEU
-
--- Permet d'extraire les coordonnees du personnage
-gameStateToCoord :: GameState -> Coordonnee
-gameStateToCoord (GameState x y s) = M.Coordonnee  (toInteger x) (toInteger y)
-
 --inicialisation  avec speed===4
-initGameState :: GameState
-initGameState = GameState 0 0 4
+initGameState :: Carte -> Envi -> Modele
+initGameState carte envi = GameState carte envi  K.createKeyboard  0 0 4
+
 
 --MouvementS   
 moveLeft :: GameState -> GameState
-moveLeft gs@(GameState x _ s)    --MOVE LEFT WITH VALUE "s"
+moveLeft gs@(GameState _ _ _ x _ s)    --MOVE LEFT WITH VALUE "s"
   | x > 0 = gs{persoX = x - s}
   | otherwise = gs
 
 moveRight :: GameState -> GameState
-moveRight gs@(GameState x _ s) 
+moveRight gs@(GameState _ _ _  x _ s) 
   | x < 540 = gs{persoX = x + s}
   | otherwise = gs
                               
 moveUp :: GameState -> GameState
-moveUp gs@(GameState _ y s) 
+moveUp gs@(GameState  _ _ _  _ y s) 
   | y > 0 = gs{persoY = y - s}
   | otherwise = gs
 
 moveDown :: GameState -> GameState
-moveDown gs@(GameState _ y s) 
+moveDown gs@(GameState _ _ _  _ y s) 
   | y < 380 = gs{persoY = y + s}
   | otherwise = gs
 
