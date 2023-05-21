a867d34c1723d070c9c3045126bfb558a7c3b63a
Lynadjdjdjd <114083371+Lynadjdjdjd@users.noreply.github.com>
2023-05-21 14:27:01 +0200

    test environnement

--- PAF_NOUS/test/Spec.hs
+++ PAF_NOUS/test/Spec.hs
@@ -1,2 +1,11 @@
+import Test.Hspec
+import CarteSpec as CS
+import EnvironementSpec as ES
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+--main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+    -- carte
+    CS.carteAllCoordsInBoundsSpec
+    CS.carteCoordSpec
+    ES.engineSpec
